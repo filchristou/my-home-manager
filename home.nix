af9c6aca5cbd254b91c8be125da2387132d401d1
Filippos Christou <filchristou@outlook.com>
2025-01-09 09:01:22 +0200

    tmux added - wezterm libGL bug

--- home.nix
+++ home.nix
@@ -3,11 +3,21 @@
 	home = {
 		packages = with pkgs; [
 			hello
+			wezterm
+			htop
+			tmux
 		];
 
 		username = "pakis";
 		homeDirectory = "/home/pakis";
 
 		stateVersion = "23.11";
+
+		file = {
+			"hello.txt".text = "Hello, world!";
+		};
+
 	};
+
+	programs.home-manager.enable = true;
 }
